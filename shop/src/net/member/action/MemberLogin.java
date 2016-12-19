5d3ab76f6dc039046e39dddd3af385e9d9136d1c
JeongSangIll <gentlesangil@nate.com>
2016-12-19 16:25:03 +0900

    member 테이블 insert,List,Detail,Update,Delete 코딩완료 Login 처리중

---
+++ shop/src/net/member/action/MemberLogin.java
@@ -0,0 +1,23 @@
+package net.member.action;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/MemberLogin")
+public class MemberLogin extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.getRequestDispatcher("/Member/MemberLoginForm.jsp").forward(request, response);
+	}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+	}
+
+}
